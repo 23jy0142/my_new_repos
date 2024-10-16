02ed24e82234067f2f2ecf1568ca5c5811993139
渡辺 <111541219+23jy0142@users.noreply.github.com>
2024-10-16 15:26:39 +0900

    日本電子専門学校の1行を追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class Helloworld{
   public static void main(String[] args){
     System.out.println("HelloWorld");
+    System.out.println("HelloWorld");
+    System.out.println("日本電子専門学校");
   }
 }
