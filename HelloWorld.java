130691adbacf8a0fe500f04bb0c8a3489a51f0a0
渡辺 <111541219+23jy0142@users.noreply.github.com>
2024-10-23 16:20:50 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,15 +2,9 @@ public class Helloworld{
   public static void main(String[] args){
     System.out.println("HelloWorld");
     System.out.println("HelloWorld");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
-    System.out.println("日本電子専門学校");
+    for(int i = 0;i < 10;i++){
+      System.out.println("日本電子専門学校");  
+    }
   }
 }
 
